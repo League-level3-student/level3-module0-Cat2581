8fc8e1cf5b5ef3dfbdf56ff0213f1ec9bb7b79f3
heidi <heidi@HDCPC>
2021-04-22 18:28:50 -0700

    commit

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,32 +5,55 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] videogames = new String[5];
+    	
+    	videogames[0] = "OverWatch";
+    	videogames[1] = "Rainbow Six Seige";
+    	videogames[2] = "Cold War";
+    	videogames[3] = "Neir Automata";
+    	videogames[4] = "Apex";
         // 2. print the third element in the array
-
+    	System.out.println(videogames[3]);
         // 3. set the third element to a different value
-
+    	videogames[3] = "Day Z";
         // 4. print the third element again
-
+    	System.out.println(videogames[3]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
 
         // 7. make an array of 50 integers
-
+    	 int[] ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 ,30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39 ,40 ,41 ,42 ,43 ,44 ,45 ,46 ,47 ,48 ,49, 50};
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+    	
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+    	 
         // 10 print the entire array to see if step 8 was correct
-
+    	 
         // 11. print the largest number in the array.
-
+    	 
         // 12. print only the last element in the array
-
+    	
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
