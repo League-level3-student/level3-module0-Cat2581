0c53f635b9bcdf63e40271b9a06ede0b17548e83
heidi <heidi@HDCPC>
2021-04-29 18:32:53 -0700

    commit

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -25,21 +25,25 @@ public class _01_IntroToArrays {
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
 
         // 7. make an array of 50 integers
-    	 int[] ints = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 ,30 ,31 ,32 ,33 ,34 ,35 ,36 ,37 ,38 ,39 ,40 ,41 ,42 ,43 ,44 ,45 ,46 ,47 ,48 ,49, 50};
-        // 8. use a for loop to make every value of the integer array a random
-        //    number
+    	Random random = new Random();
+        int intArray = Integer.MAX_VALUE;
+    	int[] intArray1 = new int[50];
+        for (int i = 0; i < intArray1.length; i++) {
+           intArray1[i] = random.nextInt(); 
+           System.out.println(intArray1[i]);
+        }
+    }
+        // 8. use a for loop to make every value of the integer array a random number
     	
-        // 9. without printing the entire array, print only the smallest number
-        //    on the array
-    	 
+        // 9. without printing the entire array, print only the smallest number on the array
+
         // 10 print the entire array to see if step 8 was correct
     	 
         // 11. print the largest number in the array.
     	 
         // 12. print only the last element in the array
-    	
     }
-}
+
 
 
 
